12a023435f0b24ba661e915fa8a3b8a060835ecb
sahilpatel18 <sahil.41801@gmail.com>
2020-05-29 12:55:47 -0400

    first commit

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
+
+
+// mongodb+srv://myuser:CQDewv98dtasz5Tw@cluster0-ztxns.mongodb.net/test?retryWrites=true&w=majority
